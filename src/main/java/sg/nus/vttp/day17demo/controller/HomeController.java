766c40bf9764aac20c81504c52aaf0fdc87763dc
Isaac Tee <iztyj.work@gmail.com>
2023-12-05 10:42:50 +0800

    initial commit

---
+++ src/main/java/sg/nus/vttp/day17demo/controller/HomeController.java
@@ -0,0 +1,11 @@
+package sg.nus.vttp.day17demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class HomeController {
+    
+    
+}
---
+++ src/test/java/sg/nus/vttp/day17demo/Day17demoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.vttp.day17demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day17demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
